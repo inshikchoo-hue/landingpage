55ef164b4ad64f7e3da35ce240c167ce7daf007f
inshikchoo-hue <inshikchoo@gmail.com>
2025-09-29 00:25:32 +0900

    20250929

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,20 +9,42 @@ closeBtn.addEventListener("click", function () {
     menu.classList.remove("on");
 });
 
-
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 'auto',
-    slidesPerGroup: 2,
-    spaceBetween: 26,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    loop: true,
+var swiper1 = new Swiper(".mySwiper1", {
+  slidesPerView: "auto",       
+  slidesPerGroup: 2,        // 한 번에 보이는 슬라이드 수
+  loop: true,             // 무한 루프
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  spaceBetween: 20,       // 슬라이드 간 간격(px)
+  pagination: {
+    el: ".swiper1-pagination",  // pagination 적용할 엘리먼트
+    clickable: true,           // 클릭 가능
+  },
+  navigation: {
+    nextEl: ".swiper1-button-next", // 필요 시 버튼 추가 가능
+    prevEl: ".swiper1-button-prev",
+  },
 });
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: "auto",       
+  slidesPerGroup: 3,        // 한 번에 보이는 슬라이드 수
+  loop: true,             // 무한 루프
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  spaceBetween: 20,       // 슬라이드 간 간격(px)
+  pagination: {
+    el: ".swiper2-pagination",  // pagination 적용할 엘리먼트
+    clickable: true,           // 클릭 가능
+  },
+  navigation: {
+    nextEl: ".swiper2-button-next", // 필요 시 버튼 추가 가능
+    prevEl: ".swiper2-button-prev",
+  },
+});
+
 
