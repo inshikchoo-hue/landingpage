c19dd92327ce0218727232e11c87b1c5174d282f
inshikchoo-hue <inshikchoo@gmail.com>
2025-10-24 18:12:51 +0900

    20251024

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,21 +9,21 @@ closeBtn.addEventListener("click", function () {
   menu.classList.remove("on");
 });
 
-// const swiper1 = new Swiper(".mySwiper1", {
-slidesPerView: "auto",
+const swiper1 = new Swiper(".mySwiper1", {
+  slidesPerView: "auto",
   slidesPerGroup: 2,        // 한 번에 보이는 슬라이드 수
-    loop: true,             // 무한 루프
-      autoplay: {
-  delay: 3000,
-    disableOnInteraction: false,
-  },
-spaceBetween: 20,       // 슬라이드 간 간격(px)
+  loop: true,             // 무한 루프
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+  spaceBetween: 20,       // 슬라이드 간 간격(px)
   pagination: {
-  el: ".swiper1-pagination",  // pagination 적용할 엘리먼트
+    el: ".swiper1-pagination",  // pagination 적용할 엘리먼트
     clickable: true,           // 클릭 가능
   },
-navigation: {
-  nextEl: ".swiper1-button-next", // 필요 시 버튼 추가 가능
+  navigation: {
+    nextEl: ".swiper1-button-next", // 필요 시 버튼 추가 가능
     prevEl: ".swiper1-button-prev",
   },
 });
